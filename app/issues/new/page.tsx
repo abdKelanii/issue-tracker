f425d45b0775be3301ac5baeb8598a94358f4f26
Abdalsalam <abdalsalam@bonus.sa>
2025-04-03 02:23:51 +0300

    Add form handling and API integration for new issue submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,21 +1,51 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { Controller, useForm } from "react-hook-form";
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
 
-const page = () => {
+interface IssueForm {
+  title: string;
+  description: string;
+}
+
+const NewPage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   return (
-    <div className="max-w-xl space-y-3">
-      <Input placeholder="Title" />
-      <SimpleMDE placeholder="Description" />
-      <Button>Submit New Issue</Button>
-    </div>
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/issues", data);
+        router.push("/issues");
+      })}
+      className="max-w-xl space-y-3"
+    >
+      <Input placeholder="Title" {...register("title")} />
+
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+
+      <Button
+        type="submit"
+        className="w-full bg-blue-500 text-white hover:bg-blue-600"
+      >
+        Submit New Issue
+      </Button>
+    </form>
   );
 };
 
-export default page;
+export default NewPage;
