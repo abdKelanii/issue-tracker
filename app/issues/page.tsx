5f7e263dc8d94e327557bea43a833777d132bae4
Abdalsalam <abdalsalam@bonus.sa>
2025-04-03 16:33:39 +0300

    Add IssuesPage component with issue listing and create issue button; implement Table component for better display

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { prisma } from "@/prisma/client";
+import Link from "next/link";
 
-const IssuesPage = () => {
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
+
   return (
-    <div>IssuesPage</div>
-  )
-}
+    <div className="">
+      <div className="mb-5">
+        <Button>
+          <Link href="/issues/new">Create Issue</Link>
+        </Button>
+      </div>
+      <div className="border rounded-lg overflow-hidden">
+        <Table>
+          <TableHeader className="bg-blue-50 ">
+            <TableRow>
+              <TableHead>Issue</TableHead>
+              <TableHead className="hidden md:table-cell">Status</TableHead>
+              <TableHead className="hidden md:table-cell">Created</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {issues.map((issue) => (
+              <TableRow key={issue.id}>
+                <TableCell>
+                  {issue.title}
+                  <div className="block md:hidden">{issue.status}</div>
+                </TableCell>
+                <TableCell className="hidden md:table-cell">
+                  {issue.status}
+                </TableCell>
+                <TableCell className="hidden md:table-cell">
+                  {issue.createdAt.toDateString()}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+};
 
-export default IssuesPage
+export default IssuesPage;
